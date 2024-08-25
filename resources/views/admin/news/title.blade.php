49ecc1a938d6247555428a9f249b887342061ba9
luthfi <alfarisi.luthfi@yahoo.com>
2024-08-25 10:23:05 +0700

    fixing title news

---
+++ resources/views/admin/news/title.blade.php
@@ -0,0 +1,3 @@
+<h1 class="h3 mb-2 text-gray-800">News & Events</h1>
+<p class="mb-3">Stay Informed and Engaged with the Latest Updates and Exciting Events. For help please refer to read <a target="_blank"
+  href="#">documentation</a>.</p>
